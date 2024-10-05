95823ebd4d2cb41547be587d7f8627a3491470d2
v.syrov <v.syrov@pix.ru>
2024-10-05 15:47:22 +0300

    Стилизованы основные плитки

--- src/Web/ClientApp/src/app/dashboard/dashboard.component.ts
+++ src/Web/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,28 +1,22 @@
 import { Component } from '@angular/core';
-import {DashboardSummaryComponent} from "../summary/dashboard-summary.component";
+import { DashboardSummaryComponent } from '../summary/dashboard-summary.component';
 import { JobByStatusTableComponent } from '../job-by-status-table/job-by-status-table.component';
 import { JobsMonthBarChartComponent } from '../jobs-month-bar-chart/jobs-month-bar-chart.component';
-import {
-  TUI_ALWAYS_DASHED,
-  TUI_ALWAYS_NONE,
-  TuiAxes,
-  TuiBarChart,
-  TuiLineChart
-} from "@taiga-ui/addon-charts";
-import {TuiCardLarge, TuiHeader} from "@taiga-ui/layout";
-import {TuiSurface, TuiTitle} from "@taiga-ui/core";
 import { ToDoListComponent } from '../to-do-list/to-do-list.component';
-import { RelevantJobsRingChartComponent } from "../relevant-jobs-ring-chart/relevant-jobs-ring-chart.component";
-
+import { RelevantJobsRingChartComponent } from '../relevant-jobs-ring-chart/relevant-jobs-ring-chart.component';
+import { CandidateCountBarChartComponent } from './candidate-count-bar-chart/candidate-count-bar-chart.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DashboardSummaryComponent, JobByStatusTableComponent, JobsMonthBarChartComponent, TuiAxes, TuiCardLarge, TuiHeader, TuiLineChart, TuiSurface, TuiTitle, TuiBarChart, ToDoListComponent, RelevantJobsRingChartComponent],
+  imports: [
+    DashboardSummaryComponent,
+    JobByStatusTableComponent,
+    JobsMonthBarChartComponent,
+    ToDoListComponent,
+    RelevantJobsRingChartComponent,
+    CandidateCountBarChartComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styles: ``,
 })
-export class DashboardComponent {
-  protected readonly horizontalLinesHandler = TUI_ALWAYS_DASHED;
-  protected readonly verticalLinesHandler = TUI_ALWAYS_NONE;
-}
+export class DashboardComponent {}
