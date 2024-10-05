36fab96bd4a8313979841709092aa92236f41da5
v.syrov <v.syrov@pix.ru>
2024-10-06 00:04:28 +0300

    Тестовая имплементация компонента с таблицей

--- src/Web/ClientApp/src/app/dashboard/job-by-status-table/job-by-status-table.component.ts
+++ src/Web/ClientApp/src/app/dashboard/job-by-status-table/job-by-status-table.component.ts
@@ -5,6 +5,14 @@ import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { TuiCheckbox } from '@taiga-ui/kit';
 import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 import { CandidatesClient } from '../../web-api-client';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  TuiTableCell,
+  TuiTableDirective,
+  TuiTableTbody, TuiTableTd,
+  TuiTableTh,
+  TuiTableThGroup, TuiTableTr,
+} from '@taiga-ui/addon-table';
 
 @Component({
   selector: 'app-job-by-status-table',
@@ -20,9 +28,31 @@ import { CandidatesClient } from '../../web-api-client';
     TuiSurface,
     TuiTextfieldControllerModule,
     TuiTitle,
+    TuiTableTbody,
+    TuiTableTh,
+    TuiTableThGroup,
+    TuiTableDirective,
+    TuiTableTd,
+    TuiTableTr,
+    TuiTableCell,
   ],
   templateUrl: './job-by-status-table.component.html',
 })
 export class JobByStatusTableComponent {
   apiClient = inject(CandidatesClient);
+  //data = toSignal(this.apiClient.getApiCandidatesCandidatesByStatusByJob(), {
+  //  initialValue: { Разработчик: [1, 2, 3, 4, 5] },
+  //});
+  data = { Разработчик: [1, 2, 3, 4, 5, 6] };
+  dataProcessed = Object.keys(this.data).map((key) => [key, ...this.data[key]]);
+  columns: ReadonlyArray<string> = [
+    'job',
+    'cvCreated',
+    'cvApproved',
+    'interviewScheduled',
+    'interviewCompleted',
+    'hired',
+    'denied',
+  ];
+  constructor() {}
 }
