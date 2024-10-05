c7400ce6520537d56efdc2c4827c3b8864ceda92
v.syrov <v.syrov@pix.ru>
2024-10-05 11:43:24 +0300

    Рефактор

--- src/Web/Endpoints/Candidates.cs
+++ src/Web/Endpoints/Candidates.cs
@@ -16,41 +16,41 @@ public class Candidates : EndpointGroupBase
             .MapPut(UpdateCandidate, "{id}")
             .MapDelete(RemoveCandidate, "{id}")
             .MapGet(GetCandidate, "{id}")
-            .MapGet(GetAllCandidatesByStatusByJob, "candidates/{jobId}")
+            .MapGet(GetAllCandidatesByStatusByJob, "candidates-by-status-by-job")
             .MapGet(GetCandidatesCountsByJobs, "get-candidates-count-by-jobs");
     }
 
-    public Task<MilkaHR.Domain.Entities.Candidate> AddCandidate(ISender sender, AddCandidateCommand command)
+    private Task<Domain.Entities.Candidate> AddCandidate(ISender sender, AddCandidateCommand command)
     {
         return sender.Send(command);
     }
 
-    public async Task<IResult> UpdateCandidate(ISender sender, int id, UpdateCandidateByIdCommand command)
+    private async Task<IResult> UpdateCandidate(ISender sender, int id, UpdateCandidateByIdCommand command)
     {
         if (id != command.Id) return Results.BadRequest();
         await sender.Send(command);
         return Results.NoContent();
     }
 
-    public async Task<IResult> RemoveCandidate(ISender sender, int id)
+    private async Task<IResult> RemoveCandidate(ISender sender, int id)
     {
         var isDeleted = await sender.Send(new RemoveCandidateCommand(id));
         return !isDeleted ? Results.NotFound() : Results.NoContent();
     }
 
-    public async Task<IResult> GetCandidate(ISender sender, int id)
+    private async Task<IResult> GetCandidate(ISender sender, int id)
     {
         var candidate = await sender.Send(new GetCandidateById(id));
         return candidate is null ? Results.NotFound() : Results.Ok(candidate);
     }
 
-    public async Task<IResult> GetAllCandidatesByStatusByJob(ISender sender)
+    private async Task<IResult> GetAllCandidatesByStatusByJob(ISender sender)
     {
         var statistics = await sender.Send(new GetAllCandidatesByStatusByJobQuery());
         return statistics is null ? Results.NotFound() : Results.Ok(statistics);
     }
 
-    public async Task<IResult> GetCandidatesCountsByJobs(ISender sender)
+    private async Task<IResult> GetCandidatesCountsByJobs(ISender sender)
     {
         var stats = await sender.Send(new GetCandidatesCountByJobCommand());
         return Results.Ok(stats);
