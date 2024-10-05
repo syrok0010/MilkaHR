385406746abdf94b597568764b268b468b6b9911
v.syrov <v.syrov@pix.ru>
2024-10-05 12:28:55 +0300

    Поправлена GetJobsCountByPriorityQuery.cs

--- src/Application/Job/Queries/GetJobsCountByPriorityQuery.cs
+++ src/Application/Job/Queries/GetJobsCountByPriorityQuery.cs
@@ -5,22 +5,21 @@ namespace MilkaHR.Application.Job.Queries;
 
 public record GetJobsCountByPriorityQuery : IRequest<List<StatisticByPriority>>;
 
-public class GetJobsCountByPriorityQueryHandler(IApplicationDbContext db)
-    : IRequestHandler<GetJobsCountByPriorityQuery, List<StatisticByPriority>>
+public class GetJobsCountByPriorityQueryHandler(IApplicationDbContext db) : IRequestHandler<GetJobsCountByPriorityQuery, List<StatisticByPriority>>
 {
     private readonly IApplicationDbContext _db = db;
     
-    public async Task<List<StatisticByPriority>> Handle(GetJobsCountByPriorityQuery request, CancellationToken cancellationToken)
+    public Task<List<StatisticByPriority>> Handle(GetJobsCountByPriorityQuery request, CancellationToken cancellationToken)
     {
-        var groupedJobs = await _db.Jobs.GroupBy(
-            x => x.Priority,
-            x => x,
-            ((level, jobs) => new StatisticByPriority(
-                level, 
-                jobs
-                    .Sum(x => x.CandidateStatuses
-                    .Count(y => y.Job.Status == JobStatus.Closed)), 
-                jobs.Sum(x => x.CandidateStatuses.Count)))).ToListAsync(cancellationToken);
-        return groupedJobs;
+        return _db.Jobs
+            .GroupBy(
+                x => x.Priority,
+                x => x,
+                (level, jobs) => new StatisticByPriority(
+                        level, 
+                        jobs.Count(x => x.Status == JobStatus.Opened), 
+                        jobs.Count()
+                )
+            ).ToListAsync(cancellationToken);
     }
 }
--- src/Application/Job/Queries/StatisticByPriority.cs
+++ src/Application/Job/Queries/StatisticByPriority.cs
@@ -2,9 +2,4 @@ using MilkaHR.Domain.Enums;
 
 namespace MilkaHR.Application.Job.Queries;
 
-public class StatisticByPriority(PriorityLevel level, int closed, int all)
-{
-    public PriorityLevel Level { get; set; } = level;
-    public int Closed { get; set; } = closed;
-    public int All { get; set; } = all;
-}
+public record StatisticByPriority(PriorityLevel Level, int Opened, int All);
