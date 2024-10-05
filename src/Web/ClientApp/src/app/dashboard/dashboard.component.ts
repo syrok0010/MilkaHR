a65d57f12b337b5ca6d63f7ea260c79d1d95d722
v.syrov <v.syrov@pix.ru>
2024-10-05 23:33:58 +0300

    Удален компонент-дубликат

--- src/Web/ClientApp/src/app/dashboard/dashboard.component.ts
+++ src/Web/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { DashboardSummaryComponent } from '../summary/dashboard-summary.component';
-import { JobByStatusTableComponent } from '../job-by-status-table/job-by-status-table.component';
 import { JobsMonthBarChartComponent } from '../jobs-month-bar-chart/jobs-month-bar-chart.component';
 import { ToDoListComponent } from '../to-do-list/to-do-list.component';
 import { RelevantJobsRingChartComponent } from '../relevant-jobs-ring-chart/relevant-jobs-ring-chart.component';
 import { CandidateCountBarChartComponent } from './candidate-count-bar-chart/candidate-count-bar-chart.component';
 import { InterviewListComponent } from './interview-list/interview-list.component';
 import { AverageJobLifetimeComponent } from './average-job-lifetime/average-job-lifetime.component';
+import { JobByStatusTableComponent } from './job-by-status-table/job-by-status-table.component';
 
 @Component({
   selector: 'app-dashboard',
--- src/Web/ClientApp/src/app/dashboard/job-by-status-table/job-by-status-table.component.ts
+++ src/Web/ClientApp/src/app/dashboard/job-by-status-table/job-by-status-table.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgForOf } from '@angular/common';
+import { TuiButton, TuiSurface, TuiTitle } from '@taiga-ui/core';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+import { TuiCheckbox } from '@taiga-ui/kit';
+import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import { CandidatesClient } from '../../web-api-client';
 
 @Component({
   selector: 'app-job-by-status-table',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgForOf,
+    TuiButton,
+    TuiCardLarge,
+    TuiCheckbox,
+    TuiHeader,
+    TuiInputModule,
+    TuiSurface,
+    TuiTextfieldControllerModule,
+    TuiTitle,
+  ],
   templateUrl: './job-by-status-table.component.html',
-  styles: ``,
 })
-export class JobByStatusTableComponent {}
+export class JobByStatusTableComponent {
+  apiClient = inject(CandidatesClient);
+}
