ee84be5bb52bb3b8f3e73e9669d3c52f8fc68210
v.syrov <v.syrov@pix.ru>
2024-10-05 14:24:41 +0300

    Рефакторинг туду

--- src/Web/ClientApp/src/app/to-do-list/to-do-list.component.ts
+++ src/Web/ClientApp/src/app/to-do-list/to-do-list.component.ts
@@ -1,35 +1,54 @@
-import { NgFor } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import {TuiCardLarge, TuiHeader} from "@taiga-ui/layout";
-import {TuiSurface, TuiTitle} from "@taiga-ui/core";
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+import { TuiButton, TuiSurface, TuiTitle } from '@taiga-ui/core';
+import { TuiInputModule } from '@taiga-ui/legacy';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TuiCheckbox } from '@taiga-ui/kit';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface Task {
   title: string;
   completed: boolean;
-
 }
 
 @Component({
   selector: 'app-to-do-list',
-  imports:[NgFor,TuiCardLarge,TuiHeader,TuiSurface],
+  imports: [
+    NgFor,
+    TuiCardLarge,
+    TuiHeader,
+    TuiSurface,
+    TuiTitle,
+    TuiButton,
+    TuiInputModule,
+    ReactiveFormsModule,
+    TuiCheckbox,
+    AsyncPipe,
+  ],
   templateUrl: './to-do-list.component.html',
-  standalone: true
+  standalone: true,
 })
 export class ToDoListComponent {
   newTask: string = '';
-  tasks: Task[] = [
+  tasks = new BehaviorSubject<Task[]>([
     { title: 'Finish report', completed: false },
     { title: 'Grocery shopping', completed: false },
-    { title: 'Call mom', completed: true }
-  ];
+    { title: 'Call mom', completed: true },
+  ]);
+  addTaskControl = new FormControl<string>('');
 
-  get allTasksCompleted(): boolean {
-    return this.tasks.every(task => task.completed);
-  }
+  allTasksCompleted$ = this.tasks.pipe(map((v) => v.every((t) => t.completed)));
 
   toggleAllTaskCompletion() {
-    const allCompleted = this.allTasksCompleted;
-    this.tasks.forEach(task => task.completed = !allCompleted);
+    const allCompleted = this.tasks.value.every((t) => t.completed);
+    this.tasks.next([
+      ...this.tasks.value.map((t) => {
+        t.completed = !allCompleted;
+        return t;
+      }),
+    ]);
   }
 
   onInputChange(event: Event) {
@@ -40,22 +59,28 @@ export class ToDoListComponent {
   }
 
   addTask() {
-    if (this.newTask.trim() !== '') {
-      this.tasks.push({ title: this.newTask, completed: false });
-      this.newTask = '';
+    if (this.addTaskControl.value.trim() !== '') {
+      this.tasks.next([
+        ...this.tasks.value,
+        { title: this.addTaskControl.value, completed: false },
+      ]);
+      this.addTaskControl.reset('');
     }
   }
 
-
-
   toggleTaskCompletion(index: number) {
-    this.tasks[index].completed = !this.tasks[index].completed;
+    this.tasks.next(
+      this.tasks.value.map((e, i) => {
+        if (i == index) e.completed = !e.completed;
+        return e;
+      }),
+    );
   }
 
   removeTask(index: number) {
-    this.tasks.splice(index, 1);
+    this.tasks.next([...this.tasks.value.filter((e, i) => i !== index)]);
   }
   clearCompletedTasks() {
-    this.tasks = this.tasks.filter(task => !task.completed);
+    this.tasks.next([...this.tasks.value.filter((e) => !e.completed)]);
   }
 }
