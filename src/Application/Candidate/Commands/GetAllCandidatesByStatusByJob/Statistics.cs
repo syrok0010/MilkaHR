2bdac7b9d564d00f622cc0f49fe7a22009347803
VladZF <vlad2003587@gmail.com>
2024-10-05 04:01:04 +0300

    added base for job stats endpoint

---
+++ src/Application/Candidate/Commands/GetAllCandidatesByStatusByJob/Statistics.cs
@@ -0,0 +1,22 @@
+namespace Microsoft.Extensions.DependencyInjection.Candidate.Commands.GetAllCandidatesByStatusByJob;
+
+public class Statistics
+{
+    public Statistics()
+    {
+        UnderInvestigation = 0;
+        Accepted = 0;
+        Rejected = 0;
+    }
+
+    public Statistics(int underInvestigation, int accepted, int rejected)
+    {
+        UnderInvestigation = underInvestigation;
+        Accepted = accepted;
+        Rejected = rejected;
+    }
+    
+    public int UnderInvestigation { get; set; }
+    public int Accepted { get; set; }
+    public int Rejected { get; set; }
+}
--- src/Web/ClientApp/src/app/web-api-client.ts
+++ src/Web/ClientApp/src/app/web-api-client.ts
@@ -294,6 +294,7 @@ export interface ICandidatesClient {
     updateCandidate(id: number, command: UpdateCandidateByIdCommand): Observable<void>;
     removeCandidate(id: number): Observable<void>;
     getCandidate(id: number): Observable<void>;
+    allCandidatesByStatusByJob(jobId: number): Observable<void>;
 }
 
 @Injectable({
@@ -340,6 +341,53 @@ export class CandidatesClient implements ICandidatesClient {
     }
 
     protected processAddCandidate(response: HttpResponseBase): Observable<Candidate> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return _observableOf(null as any);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf(null as any);
+    }
+
+    allCandidatesByStatusByJob(jobId: number): Observable<void> {
+        let url_ = this.baseUrl + "/api/Candidates/candidates/{jobId}";
+        if (jobId === undefined || jobId === null)
+            throw new Error("The parameter 'jobId' must be defined.");
+        url_ = url_.replace("{jobId}", encodeURIComponent("" + jobId));
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+            })
+        };
+
+        return this.http.request("get", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processAllCandidatesByStatusByJob(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processAllCandidatesByStatusByJob(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<void>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<void>;
+        }));
+    }
+
+    protected processAllCandidatesByStatusByJob(response: HttpResponseBase): Observable<void> {
         const status = response.status;
         const responseBlob =
             response instanceof HttpResponse ? response.body :
