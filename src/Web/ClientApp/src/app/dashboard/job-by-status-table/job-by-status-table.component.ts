f83465f8f62dde696227d5c431ba7ac0385cd5b6
v.syrov <v.syrov@pix.ru>
2024-10-05 23:20:22 +0300

    Новый компонент для таблицы

---
+++ src/Web/ClientApp/src/app/dashboard/job-by-status-table/job-by-status-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-by-status-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-by-status-table.component.html',
+  styles: ``,
+})
+export class JobByStatusTableComponent {}
