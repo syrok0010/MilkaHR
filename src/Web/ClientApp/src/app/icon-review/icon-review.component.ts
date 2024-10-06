fb4f5b7fd92abfa449d39e2989428cf3d4636fbf
Полина <polinabog475@gmail.com>
2024-10-06 16:07:18 +0300

    Создала компоненты иконки отзыва и CV

---
+++ src/Web/ClientApp/src/app/icon-review/icon-review.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { TuiIcon, TuiSurface} from '@taiga-ui/core';
+import { TuiTile } from '@taiga-ui/kit';
+import { TuiCardLarge } from '@taiga-ui/layout';
+
+@Component({
+  selector: 'app-icon-review',
+  standalone: true,
+  imports: [TuiIcon,TuiTile,TuiCardLarge,TuiSurface],
+  templateUrl: './icon-review.component.html',
+
+})
+export class IconReviewComponent {
+
+}
