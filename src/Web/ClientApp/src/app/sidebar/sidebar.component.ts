22d264dedf6d9a340ae4944eb2bbcb18b1a86b6e
Tatyanka25 <lapshinatatyana25@mail.ru>
2024-10-06 19:14:17 +0300

    Кнопки импорта и экспорта

--- src/Web/ClientApp/src/app/sidebar/sidebar.component.ts
+++ src/Web/ClientApp/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TuiCardLarge } from '@taiga-ui/layout';
-import { TuiSurface } from '@taiga-ui/core';
+import { TuiButton, TuiIcon, TuiSurface } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, TuiCardLarge, TuiSurface],
+  imports: [RouterLink, TuiCardLarge, TuiSurface, TuiButton, TuiIcon],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {}
