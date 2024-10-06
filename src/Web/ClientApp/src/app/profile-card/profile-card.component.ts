d282c38c632552835db95c8f84930635717fac00
Tatyanka25 <lapshinatatyana25@mail.ru>
2024-10-06 10:50:14 +0300

    изменения в карточках

--- src/Web/ClientApp/src/app/profile-card/profile-card.component.ts
+++ src/Web/ClientApp/src/app/profile-card/profile-card.component.ts
@@ -1,10 +1,11 @@
 import { NgForOf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { TuiTagModule } from '@taiga-ui/legacy';
 import { TuiButton, TuiTitle, TuiSurface, TuiOption } from '@taiga-ui/core';
 import { TuiCardLarge } from '@taiga-ui/layout';
 import { TuiBadge } from '@taiga-ui/kit';
 import { Candidate } from '../web-api-client';
+import { TuiDay } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-profile-card',
@@ -29,4 +30,13 @@ export class ProfileCardComponent {
     'is an open-source library',
     'for awesome people',
   ];
+
+  age = computed(() => {
+    const c = this.candidate();
+    const today = TuiDay.currentUtc();
+    const daysBetween = TuiDay.lengthBetween(today, today);
+    const years = Math.floor(daysBetween / 365.25)
+    return years;
+  })
+
 }
