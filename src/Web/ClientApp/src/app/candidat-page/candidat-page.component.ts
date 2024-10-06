cf1a8dcdc645aa1ea68c4bc466597a549fbe4794
Tatyanka25 <lapshinatatyana25@mail.ru>
2024-10-06 15:02:26 +0300

    Изменения в профиле кандидата

--- src/Web/ClientApp/src/app/candidat-page/candidat-page.component.ts
+++ src/Web/ClientApp/src/app/candidat-page/candidat-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CandidateInfoComponent } from '../candidate-info/candidate-info.component';
+import { CandidateVacancyComponent } from '../candidate-vacancy/candidate-vacancy.component';
 
 @Component({
   selector: 'app-candidat-page',
   standalone: true,
-  imports: [CandidateInfoComponent],
+  imports: [CandidateInfoComponent, CandidateVacancyComponent],
   templateUrl: './candidat-page.component.html',
   styles: ``
 })
--- src/Web/ClientApp/src/app/candidate-vacancy/candidate-vacancy.component.ts
+++ src/Web/ClientApp/src/app/candidate-vacancy/candidate-vacancy.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { TuiSurface } from '@taiga-ui/core';
 import { TuiBadge, TuiTile } from '@taiga-ui/kit';
-import { TuiCardLarge } from '@taiga-ui/layout';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-candidate-vacancy',
   standalone: true,
-  imports: [TuiBadge,TuiSurface,TuiCardLarge,TuiTile],
+  imports: [TuiBadge,TuiSurface,TuiCardLarge,TuiTile,TuiHeader],
   templateUrl: './candidate-vacancy.component.html',
 
 })
