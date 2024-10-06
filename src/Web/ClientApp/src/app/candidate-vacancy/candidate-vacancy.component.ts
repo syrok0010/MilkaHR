83597f3c4db4388b4b969c0b8e6ee1874c2e1401
Полина <polinabog475@gmail.com>
2024-10-06 13:03:05 +0300

    Добавила candiddate-vacancy и удалила из candidate-info

---
+++ src/Web/ClientApp/src/app/candidate-vacancy/candidate-vacancy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TuiBadge } from '@taiga-ui/kit';
+
+@Component({
+  selector: 'app-candidate-vacancy',
+  standalone: true,
+  imports: [TuiBadge],
+  templateUrl: './candidate-vacancy.component.html',
+
+})
+export class CandidateVacancyComponent {
+
+}
