b851363ca29109791e47862213215d9c74dc4006
Tatyanka25 <lapshinatatyana25@mail.ru>
2024-10-06 12:38:04 +0300

    Изменение карточки вакансии и карточки кандидата

--- src/Web/ClientApp/src/app/job-card/job-card.component.ts
+++ src/Web/ClientApp/src/app/job-card/job-card.component.ts
@@ -1,14 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { TuiSurface } from '@taiga-ui/core';
+import { TuiSurface, TuiTitle } from '@taiga-ui/core';
 import { TuiDataListWrapper, TuiFilterByInputPipe } from '@taiga-ui/kit';
-import { TuiCardLarge } from '@taiga-ui/layout';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { TuiInputModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'app-job-card',
   standalone: true,
-  imports: [TuiCardLarge, TuiSurface, ReactiveFormsModule, TuiDataListWrapper, TuiFilterByInputPipe, TuiInputModule],
+  imports: [TuiCardLarge, TuiSurface, ReactiveFormsModule, TuiDataListWrapper, TuiFilterByInputPipe, TuiInputModule, TuiTitle, TuiHeader],
   templateUrl: './job-card.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
