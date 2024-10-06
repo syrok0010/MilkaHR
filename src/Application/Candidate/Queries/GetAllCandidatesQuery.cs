cb9af3794ba0a43917148ecd78f85e5805b1ccbb
Valeria Lapshina <lapshina_valeria@mail.ru>
2024-10-06 15:10:01 +0300

    Поправлена квиря на получение всех кандидатов

--- src/Application/Candidate/Queries/GetAllCandidatesQuery.cs
+++ src/Application/Candidate/Queries/GetAllCandidatesQuery.cs
@@ -4,9 +4,11 @@ using MilkaHR.Domain.Enums;
 namespace MilkaHR.Application.Candidate.Queries;
 
 public record GetAllCandidatesQuery(
+    string? FullName,
     int? AgeFrom = null,
     int? AgeTo = null,
-    int? WorkExperience = null,
+    int? WorkExperienceFrom = null,
+    int? WorkExperienceTo = null,
     string[]? Tags = null,
     string[]? JobTitles = null,
     CandidateStatus[]? Statuses = null): IRequest<IEnumerable<Domain.Entities.Candidate>>;
@@ -25,26 +27,33 @@ public class GetAllCandidatesQueryHandler(IApplicationDbContext db)
             .Include(x => x.JobStatuses)
             .ThenInclude(x => x.Job)
             .AsQueryable();
-
+        
+        if (request.FullName is not null) 
+            c = c.Where(x => (x.Name + " " + x.LastName).Contains(request.FullName) ||
+                             (x.LastName + " " + x.Name).Contains(request.FullName));
+        
         if (request.AgeFrom is not null) 
             c = c.Where(x => Math.Floor((DateTime.UtcNow - x.BirthDate).TotalDays / 365.25) >= request.AgeFrom);
         
         if (request.AgeTo is not null) 
             c = c.Where(x => Math.Floor((DateTime.UtcNow - x.BirthDate).TotalDays / 365.25) <= request.AgeTo);
 
-        if (request.WorkExperience is not null) 
-            c = c.Where(x => x.WorkExperience == request.WorkExperience);
+        if (request.WorkExperienceFrom is not null) 
+            c = c.Where(x => x.WorkExperience >= request.WorkExperienceFrom);
+        
+        if (request.WorkExperienceTo is not null) 
+            c = c.Where(x => x.WorkExperience <= request.WorkExperienceTo);
 
         if (request.Tags is not null && request.Tags.Length > 0) 
-            c = c.Where(x => request.Tags.All(t => x.Tags.Contains(t)));
+            c = c.Where(x => request.Tags.Any(t => x.Tags.Contains(t)));
         
         if (request.JobTitles is not null && request.JobTitles.Length > 0) 
             c = c.Where(x => request.JobTitles
-                .All(t => x.JobStatuses.Any(j => j.Job.Title == t)));
+                .Any(t => x.JobStatuses.Any(j => j.Job.Title == t)));
         
         if (request.Statuses is not null && request.Statuses.Length > 0) 
             c = c.Where(x => request.Statuses
-                .All(t => x.JobStatuses.Any(j => j.ProcessingStatus == t)));
+                .Any(t => x.JobStatuses.Any(j => j.ProcessingStatus == t)));
 
         return Task.FromResult<IEnumerable<Domain.Entities.Candidate>>(c);
     }
